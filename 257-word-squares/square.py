0245978761bcf4106a927d96302731efb88a1060
Tim Clark <timeeeee@gmail.com>
2016-03-09 23:44:49 -0600

    much faster 257- filter words we don't have characters for

--- 257-word-squares/square.py
+++ 257-word-squares/square.py
@@ -11,7 +11,7 @@ from trie import Trie
 DICT_FILE = "enable1.txt"
 
 
-def guess(grid, trie, space, chars_left):
+def guess_chars(grid, trie, space, chars_left):
     # If no characters left, grid is full
     if chars_left == "":
         print "========== FOUND SOLUTION =========="
@@ -47,22 +47,48 @@ def guess(grid, trie, space, chars_left):
     for char in valid_chars:
         # plop the guess into the grid
         grid[row][col] = char
-        results += guess(grid, trie, space + 1, chars_left.replace(char, "", 1))
+        results += guess_chars(grid, trie, space + 1, chars_left.replace(char, "", 1))
 
     return results
+
+
+"""
+The way to do this which makes much more sense is guessing a word at a time
+instead of a single character. The problem then is how to 
+
+Is there a data structure that would make it efficient to find everything with
+character counts less than or equal to some set of numbers? Rather than step
+through the whole list each time, could generate smaller lists to pass to
+recursively called functions?
+
+Make character counts for each word at the beginning. For remaining characters,
+remember counts for the whole alphabet, not a string. MAY AS WELL DO THIS IN C.
+"""
+
+
+def guess_words(words_so_far, trie, word_list, char_counts_left):
+    if len(words_so_far) == len(words_so_far[0]):
+        # Done!
+        return [words_so_far]
+
+    pass
 
 
 def make_square(string):
     size_str, chars = string.split()
     size = int(size_str)
-    trie = Trie(DICT_FILE, size)
+    trie = Trie(DICT_FILE, size, filter_chars=chars)
 
     grid = [[None for _ in range(size)] for _ in range(size)]
-    return guess(grid, trie, 0, chars)
+    return guess_chars(grid, trie, 0, chars)
 
 
 if __name__ == "__main__":
-#    make_square("4 aaccdeeeemmnnnoo")
-#    make_square("5 aaaeeeefhhmoonssrrrrttttw")
-#    make_square("5 aabbeeeeeeeehmosrrrruttvv")
-    make_square("7 aaaaaaaaabbeeeeeeedddddggmmlloooonnssssrrrruvvyyy")
+    problems = [
+        "4 aaccdeeeemmnnnoo",
+        "5 aaaeeeefhhmoonssrrrrttttw",
+        "5 aabbeeeeeeeehmosrrrruttvv",
+        "7 aaaaaaaaabbeeeeeeedddddggmmlloooonnssssrrrruvvyyy"]
+    for problem in problems:
+        print problem
+        make_square(problem)
--- 257-word-squares/trie.py
+++ 257-word-squares/trie.py
@@ -1,14 +1,40 @@
+def can_spell(available, word):
+    """Check if a word can be spelled using characters in available"""
+    for char in set(word):
+        if word.count(char) > available.count(char):
+            return False
+    return True
+
+
+def test_can_spell():
+    from string import printable
+    from random import sample
+    for _ in xrange(1000):
+        word = "".join(sample(printable, 20))
+        assert(can_spell(printable, word))
+        assert(not(can_spell(printable, word + word[0])))
+        assert(can_spell("".join(sorted(printable * 2)), word * 2))
+        assert(not(can_spell("".join(sorted(printable * 2)), word * 3)))
+
+
 class Trie:
-    def __init__(self, dict_file=None, word_length=None):
+    def __init__(self, dict_file=None, word_length=None, filter_chars=None):
         self.subtries = {}
         self.word_ends_here = False
+        self.length = 0
 
         if dict_file is not None:
             with open(dict_file) as dictionary:
                 for line in dictionary:
                     word = line.strip()
                     if word.isalpha() and len(word) == word_length:
-                        self.add_word(word)
+                        if filter_chars is None or can_spell(filter_chars, word):
+                            self.length += 1
+                            self.add_word(word)
+
+
+    def __len__(self):
+        return self.length
 
     def add_word(self, word):
         """Add word to the trie"""
